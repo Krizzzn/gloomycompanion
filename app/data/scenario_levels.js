6463d243284dbe7ab88d6b7068452e5dabdaf164
Christian Peterek <christian.peterek@gmail.com>
2018-02-06 22:33:12 +0100

    Add Scenario Level data

---
+++ app/data/scenario_levels.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const SCENARIO_LEVELS = {
+  0: {
+    "Level": 0,
+    "Monster Level": 0,
+    "Gold Conversion": 2,
+    "Trap Damage": 2,
+    "XP": 4
+  },
+  1: {
+    "Level": 1,
+    "Monster Level": 1,
+    "Gold Conversion": 2,
+    "Trap Damage": 3,
+    "XP": 6
+  },
+  2: {
+    "Level": 2,
+    "Monster Level": 2,
+    "Gold Conversion": 3,
+    "Trap Damage": 4,
+    "XP": 8
+  },
+  3: {
+    "Level": 3,
+    "Monster Level": 3,
+    "Gold Conversion": 3,
+    "Trap Damage": 5,
+    "XP": 10
+  },
+  4: {
+    "Level": 4,
+    "Monster Level": 4,
+    "Gold Conversion": 4,
+    "Trap Damage": 6,
+    "XP": 12
+  },
+  5: {
+    "Level": 5,
+    "Monster Level": 5,
+    "Gold Conversion": 4,
+    "Trap Damage": 7,
+    "XP": 14
+  },
+  6: {
+    "Level": 6,
+    "Monster Level": 6,
+    "Gold Conversion": 5,
+    "Trap Damage": 8,
+    "XP": 16
+  },
+  7: {
+    "Level": 7,
+    "Monster Level": 7,
+    "Gold Conversion": 6,
+    "Trap Damage": 9,
+    "XP": 18
+  }
+};
+
+export { SCENARIO_LEVELS };
